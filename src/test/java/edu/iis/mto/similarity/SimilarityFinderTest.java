ea76dcfc673a0a66e6aaf5471d5ce1b5c4b79b7d
H22A5 <bartlomiej.pajak20@gmail.com>
2021-03-22 18:00:11 +0100

    Added test for checking if sequences' length are zero.

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,9 +1,26 @@
 package edu.iis.mto.similarity;
 
-import static org.junit.jupiter.api.Assertions.fail;
-
+import edu.iis.mto.searcher.SearchResult;
+import edu.iis.mto.searcher.SequenceSearcher;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SimilarityFinderTest {
 
+    @Test
+    public void shouldReturnOneWhenSequenceLengthsAreZero() {
+        SimilarityFinder finder = new SimilarityFinder(new SequenceSearcher() {
+            @Override
+            public SearchResult search(int elem, int[] sequence) {
+                return null;
+            }
+        });
+        int[] seq1 = {};
+        int[] seq2 = {};
+
+        double result = finder.calculateJackardSimilarity(seq1, seq2);
+
+        assertEquals(1.0d, result);
+    }
 }
